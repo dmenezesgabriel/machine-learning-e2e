556c2b76c2233c926c0b8b3dec737a0009aa6bfd
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-10-30 23:23:00 -0300

    chore: add folder structure

---
+++ src/exception.py
@@ -0,0 +1,32 @@
+import types
+
+
+def error_message_detail(
+    message: Exception, error_detail: types.ModuleType
+) -> str:
+    _, _, exc_tb = error_detail.exc_info()  # type: ignore[attr-defined]
+    error_message = (
+        f"Error occurred in script: "
+        f"{exc_tb.tb_frame.f_code.co_filename}, "
+        f"line number: {exc_tb.tb_lineno}, "
+        f"error message: {str(message)}"
+    )
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, message: Exception, error_detail: types.ModuleType):
+        super().__init__(message)
+        self.error_message = error_message_detail(message, error_detail)
+
+    def __str__(self) -> str:
+        return self.error_message
+
+
+if __name__ == "__main__":
+    import sys
+
+    try:
+        a = 1 / 0
+    except Exception as error:
+        raise CustomException(error, sys)
---
+++ src/logger.py
@@ -0,0 +1,38 @@
+import logging
+import os
+from datetime import datetime
+from logging.handlers import RotatingFileHandler
+
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+number_in_megabytes = 5 * 1024 * 1024  # 5MB
+number_of_rotating_files = 3
+
+file_handler = RotatingFileHandler(
+    LOG_FILE_PATH,
+    maxBytes=number_in_megabytes,
+    backupCount=number_of_rotating_files,
+)
+
+stream_handler = logging.StreamHandler()
+
+formatter = logging.Formatter(
+    "[ %(asctime)s ] [%(levelname)s] %(name)s:%(lineno)d - %(message)s"
+)
+
+file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+if __name__ == "__main__":
+    logger.info("Logger initialized successfully.")
+    logger.warning("This is a warning message.")
+    logger.error("This is an error message.")
